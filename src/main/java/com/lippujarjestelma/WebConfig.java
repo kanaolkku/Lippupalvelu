cb9468822c32553f229f57fbb568214d2dd6630c
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2021-12-14 21:23:26 +0200

    style changes

--- src/main/java/com/lippujarjestelma/WebConfig.java
+++ src/main/java/com/lippujarjestelma/WebConfig.java
@@ -3,6 +3,7 @@ package com.lippujarjestelma;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,12 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("PUT", "DELETE", "POST", "GET");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!registry.hasMappingForPattern("/css/**")) {
+			registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+		}
 	}
 }
