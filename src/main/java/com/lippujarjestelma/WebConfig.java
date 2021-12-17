76ecac597b8ed1f00b6ccd90469a47f5b50bdcf4
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2021-12-17 02:48:26 +0200

    cors acting up again

--- src/main/java/com/lippujarjestelma/WebConfig.java
+++ src/main/java/com/lippujarjestelma/WebConfig.java
@@ -3,7 +3,6 @@ package com.lippujarjestelma;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,10 +12,5 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("PUT", "DELETE", "POST", "GET");
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 }
